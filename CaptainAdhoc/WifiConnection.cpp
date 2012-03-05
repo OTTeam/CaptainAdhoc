e9c9accd93855f8e6c130c305f6ac99ac8f72d0c
Kevin Vandel <vandel.kevin@gmail.com>
2012-03-05 09:25:32 +0100

    de toute facon personne ne lit les logs :p

--- CaptainAdhoc/WifiConnection.cpp
+++ CaptainAdhoc/WifiConnection.cpp
@@ -73,10 +73,45 @@ void WifiConnection::Connect()
         _network->RegisterNetworkNotifications();
     }
 
+    connect(_network, SIGNAL(ConnectionStatusChanged(int)), this, SIGNAL(ConnectionStatusChanged(int)));
     connect(_network, SIGNAL(ConnectionStatusChanged(int)), this, SLOT(onConnectionStatusChanged(int)));
+    connect(_network, SIGNAL(ConnectionFail(int)), this, SIGNAL(ConnectionFail(int)));
+    connect(_network, SIGNAL(ConnectionFail(int)), this, SLOT(onConnectionFail(int)));
 }
 
 void WifiConnection::Disconnect()
 {
     _network->Disconnect();
+}
+
+void WifiConnection::onConnectionStatusChanged(int status)
+{
+    switch (status)
+    {
+    case FORMED:
+        qDebug() << "Notification received : network formed";
+        break;
+    case CONNECTED:
+        qDebug() << "Notification received : connected to network";
+        break;
+    case DISCONNECTED:
+        qDebug() << "Notification received : disconnected from network";
+        break;
+    }
+}
+
+void WifiConnection::onConnectionFail(int reason)
+{
+    switch(reason)
+    {
+    case DOT11_ADHOC_CONNECT_FAIL_DOMAIN_MISMATCH:
+        qDebug() << "Notification received : connection fail (domain mismatch)";
+        break;
+    case DOT11_ADHOC_CONNECT_FAIL_PASSPHRASE_MISMATCH:
+        qDebug() << "Notification received : connection fail (pwd mismatch)";
+        break;
+    case DOT11_ADHOC_CONNECT_FAIL_OTHER:
+        qDebug() << "Notification received : connection fail";
+        break;
+    }
 }
--- CaptainAdhoc/WifiConnection.h
+++ CaptainAdhoc/WifiConnection.h
@@ -16,6 +16,14 @@ public slots:
     void Connect();
     void Disconnect();
 
+signals:
+    void ConnectionStatusChanged(int);
+    void ConnectionFail(int);
+
+private slots:
+    void onConnectionStatusChanged(int);
+    void onConnectionFail(int);
+
 private:
     WifiManager * _manager;
     WifiNetwork * _network;
